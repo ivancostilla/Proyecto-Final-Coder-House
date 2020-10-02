9415ca5680c20c149bdd496feb86d09b63c12659
ivancostilla <ivan20punto88@gmail.com>
2020-10-01 23:34:44 -0300

    23:33 1/10/2020 terminado slos index.html, rooms.html y about.html

--- root/dist/scripts/scripts.js
+++ root/dist/scripts/scripts.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 const contactOff = document.getElementById('contact-menu-off');
+const acordeon = document.getElementsByClassName('acordeon__contenedor-box');
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
@@ -16,3 +17,9 @@ contactOff.addEventListener("click", () => {
         link.classList.toggle("fade");
     });
 });
+
+for (i = 0; i < acordeon.length; i++) {
+    acordeon[i].addEventListener('click', function() {
+        this.classList.toggle('active')
+    })
+};
