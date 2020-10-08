0b36b025e0af45ad18906bf6385117dfe1d6fea2
ivancostilla <ivan20punto88@gmail.com>
2020-10-08 01:24:53 -0300

    pruebas con swiper.js

--- root/dist/scripts/index.js
+++ root/dist/scripts/index.js
@@ -76,3 +76,24 @@ sr.reveal('.sections-scroll-bottom-3', {
     origin: 'bottom',
     distance: '500px',
 });
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
